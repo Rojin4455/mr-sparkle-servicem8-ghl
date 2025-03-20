99145cf85b5f56537604efcc54da11c6cc8d30ef
Rojin Saji <rojinsaji@Rojins-MacBook-Air.local>
2025-03-20 18:38:53 +0530

    added requirements.txt file and added public ip in allowed hosts

--- sparklecleaning/settings.py
+++ sparklecleaning/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '16.16.128.154'
+]
 
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
