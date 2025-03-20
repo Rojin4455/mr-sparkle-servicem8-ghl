ca454151b6ff1743a10807498568db127d006eb1
Rojin Saji <rojinsaji@Rojins-MacBook-Air.local>
2025-03-21 02:43:55 +0530

    added *

--- sparklecleaning/settings.py
+++ sparklecleaning/settings.py
@@ -31,13 +31,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '16.16.128.154',
-    "0015-2401-4900-8825-7bac-9cd9-150d-de32-bc45.ngrok-free.app",
-    "webhook.mrsparklewindowcleaning.com.au"
-]
+ALLOWED_HOSTS = ["*"]
 
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
